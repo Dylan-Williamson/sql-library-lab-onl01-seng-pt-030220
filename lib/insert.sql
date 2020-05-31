83a99b3053b988e5a6797987a1980410c5db372b
Dylan Williamson <dirtyassweed@icloud.com>
2020-05-31 18:13:37 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,12 +1,12 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "South Park", 1, 1), (2, "Second Series", 2, 2);
 
-INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
+INSERT INTO subgenres (id, name) VALUES (1, "comedy"), (2, "random");
 
-INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
+INSERT INTO authors (id, name) VALUES (1, "Trey Parker"), (2, "Matt Stone");
 
-INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Butters' house", 1996, 1), (2, "Lemmiwinks", 1998, 1), (3, "A Storm of Mehecans", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
-INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1), (2, "Tyrion Lanister", "A Lanister always pays is debts", "human", 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), (4, "Eddard Stark", "Winter is coming", "human", 1);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Butters", "Aw", "boy", 1), (2, "Kenny", "Hmmfmmhmmdemmfmm", "human", 1), (3, "Cartman", "Screw you kyle", "human", 1), (4, "Chef", "Winter is coming", "human", 1);
 
 INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Character One", "motto one", "cylon", 2), (6, "Character Two", "motto two", "human", 2), (7, "Character Three", "motto three", "cylon", 2), (8, "Character Four", "motto four", "cylon", 2);
 
